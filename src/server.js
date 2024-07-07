db1f2e3ab2ef368e461c8536fed5e43d6a1d5865
satria97 <satriagarlan97@gmail.com>
2024-07-07 11:40:27 +0700

    feat: Register Notes Plugin in Hapi Server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+// const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+  // buat instance dari NotesService dengan nama notesService.
+  const notesService = new NotesService();
+
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -12,7 +18,16 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  // Setelah plugin notes di daftarkan, kita bisa menghapus penggunaan routes lama
+  // server.route(routes);
+
+  // daftarkan plugin notes dengan options.service bernilai notesService menggunakan perintah await server.register
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
