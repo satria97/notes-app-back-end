5e81424d8612edd7fa4a9a04439710941c1cddfa
satria97 <satriagarlan97@gmail.com>
2024-07-11 20:39:52 +0700

    setup db

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
@@ -10,8 +13,8 @@ const init = async () => {
 
 
   const server = Hapi.server({
-    port: 3000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
