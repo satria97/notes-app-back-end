9372ad25b90ec7e288986480c57371c5be40c2af
satria97 <satriagarlan97@gmail.com>
2024-07-12 09:27:50 +0700

    feat: Create Update and Delete Notes Function

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -54,4 +54,31 @@ class NotesService {
         }
         return result.rows.map(mapDBToModel)[0];
     }
+
+    async editNoteById(id, {title, body, tags}) {
+        const updatedAt = new Date().toISOString();
+        // lakukan query untuk mengubah note di dalam database berdasarkan id yang diberikan.
+        const query = {
+            text: 'UPDATE notes SET title = $1, body = $2, tags = $3, updated_at = $4 WHERE id = $5 RETURNING id',
+            values: [title, body, tags, updatedAt, id],
+        };
+        const result =  await this._pool.query(query);
+
+        // periksa nilai result.rows bila nilainya 0 (false) maka bangkitkan NotFoundError. 
+        if (!result.rows.length) {
+            throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
+        }
+    }
+
+    async deleteNoteById(id) {
+        const query = {
+            text: 'DELETE FROM notes WHERE id = $ RETURNING id',
+            values: [id],
+        };
+        const result = await this._pool.query(query);
+        if (!result.rows.length) {
+            throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
+        }
+    }
 }
+module.exports = NotesService;
