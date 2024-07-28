32d1eb7b2dc1b9d65e89554d288da2f29c0cdef1
satria97 <satriagarlan97@gmail.com>
2024-07-28 08:25:16 +0700

    feat: create Notes Protect Resources

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const ClientError = require('./exceptions/ClientError');
 
 // notes
@@ -38,6 +39,31 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  // registrasikan Jwt sebagai plugin
+  // registrasi plugin eksternal
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('notesapp_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   // Setelah plugin notes di daftarkan, kita bisa menghapus penggunaan routes lama
